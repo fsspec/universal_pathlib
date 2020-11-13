d52378b8405de800bea40e6be23180c9675f091e
Andrew Fulton <andrewfulton9@gmail.com>
2020-11-13 15:32:18 -0700

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="universal_pathlib",
+    version="0.0.1",
+    author="Andrew Fulton",
+    author_email="andrewfulton9@gmail.com",
+    url="https://github.com/Quansight/universal_pathlib",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7"
+)
