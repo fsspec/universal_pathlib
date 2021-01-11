076f48d1a1089ce21b860114673587c0201e1098
tonyfast <tony.fast@gmail.com>
2021-01-11 15:02:15 -0500

    add github actions to the project

---
+++ noxfile.py
@@ -0,0 +1,19 @@
+import nox
+
+
+@nox.session(python=False)
+def develop(session):
+    session.install("flit")
+    session.run(*"flit install -s".split())
+
+
+@nox.session(python=False)
+def install(session):
+    session.install(".")
+
+
+@nox.session(reuse_venv=True)
+def test(session):
+    session.install(*"pytest".split())
+    session.install(".[test] --no-deps".split())
+    session.run("pytest")
