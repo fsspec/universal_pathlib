80904ef8217c38d91f8a707148522fec28123bf2
Andreas Poehlmann <andreas@poehlmann.io>
2022-04-06 11:46:50 +0200

    Support Python 3.10 (#43)
    * ci: run tests against Python 3.10
    
    * upath.core: fix implementation to support Python 3.10
    
    See: https://bugs.python.org/issue40038
    
    This commit removes the private internally only used `._init` method
    and restores the classmethods `_from_parts` and `_from_parsed_parts`.
    It also allows passing kwargs to those classmethods, which in turn
    requires to provide `_make_child`, `_make_child_relpath` and `parents`.
    
    Currently it doesn't cache `_accessor` and `fs`, which could be easily
    implemented.
    
    After this commit python versions 3.7 to 3.10 are supported.
    
    * flake8: fix line too long
    
    * Add UPath.stat to disinclude follow_symlinks kwarg now in pathlib
    
    * update environment for 310
    
    * updates gcs and s3 paths to work with py310 updates
    
    * black formatting
    
    * upath.core: cache the accessor instance
    
    We emulate cached_property on the _accessor attribute to lazily
    instantiate the accessor via __getattr__. This is done because, we
    can't conveniently use cached_property because UPath uses __slots__,
    which doesn't support descriptors without significantly complicating
    the code.
    
    Co-authored-by: Andrew Fulton <andrewfulton9@gmail.com>

--- upath/core.py
+++ upath/core.py
@@ -102,7 +102,7 @@ class UPathMeta(ABCMeta):
 
 class UPath(pathlib.Path, PureUPath, metaclass=UPathMeta):
 
-    __slots__ = ("_url", "_kwargs", "_closed", "fs")
+    __slots__ = ("_url", "_kwargs", "_closed", "_accessor")
 
     not_implemented = [
         "cwd",
@@ -140,12 +140,11 @@ class UPath(pathlib.Path, PureUPath, metaclass=UPathMeta):
                     if os.name == "nt"
                     else pathlib.PosixPath
                 )
-                self = cls._from_parts(args, init=False)
+                self = cls._from_parts(args)
                 if not self._flavour.is_supported:
                     raise NotImplementedError(
                         "cannot instantiate %r on your system" % (cls.__name__,)
                     )
-                self._init()
             else:
                 import upath.registry
 
@@ -153,32 +152,20 @@ class UPath(pathlib.Path, PureUPath, metaclass=UPathMeta):
                 kwargs["_url"] = parsed_url
                 args_list.insert(0, parsed_url.path)
                 args = tuple(args_list)
-                self = cls._from_parts_init(args, init=False)
-                self._init(*args, **kwargs)
+                self = cls._from_parts(args, **kwargs)
         else:
             self = super().__new__(*args, **kwargs)
         return self
 
-    def _init(self, *args, template=None, **kwargs):
-        self._closed = False
-        if not kwargs:
-            kwargs = dict(**self._kwargs)
+    def __getattr__(self, item):
+        if item == "_accessor":
+            # cache the _accessor attribute on first access
+            kw = self._kwargs.copy()
+            kw.pop("_url", None)
+            self._accessor = _accessor = self._default_accessor(self._url, **kw)
+            return _accessor
         else:
-            self._kwargs = dict(**kwargs)
-        self._url = kwargs.pop("_url") if kwargs.get("_url") else None
-
-        if not self._root:
-            if not self._parts:
-                self._root = "/"
-            elif self._parts[0] == "/":
-                self._root = self._parts.pop(0)
-        if getattr(self, "_str", None):
-            delattr(self, "_str")
-        if template is not None:
-            self._accessor = template._accessor
-        else:
-            self._accessor = self._default_accessor(self._url, *args, **kwargs)
-        self.fs = self._accessor._fs
+            raise AttributeError(item)
 
     def __getattribute__(self, item):
         if item == "__class__":
@@ -187,6 +174,21 @@ class UPath(pathlib.Path, PureUPath, metaclass=UPathMeta):
             raise NotImplementedError(f"UPath has no attribute {item}")
         else:
             return super().__getattribute__(item)
+
+    def _make_child(self, args):
+        drv, root, parts = self._parse_args(args, **self._kwargs)
+        drv, root, parts = self._flavour.join_parsed_parts(
+            self._drv, self._root, self._parts, drv, root, parts
+        )
+        return self._from_parsed_parts(drv, root, parts, **self._kwargs)
+
+    def _make_child_relpath(self, part):
+        # This is an optimization used for dir walking.  `part` must be
+        # a single part relative to this path.
+        parts = self._parts + [part]
+        return self._from_parsed_parts(
+            self._drv, self._root, parts, **self._kwargs
+        )
 
     def _format_parsed_parts(self, drv, root, parts):
         if parts:
@@ -216,6 +218,19 @@ class UPath(pathlib.Path, PureUPath, metaclass=UPathMeta):
 
     def open(self, *args, **kwargs):
         return self._accessor.open(self, *args, **kwargs)
+
+    @property
+    def parent(self):
+        """The logical parent of the path."""
+        drv = self._drv
+        root = self._root
+        parts = self._parts
+        if len(parts) == 1 and (drv or root):
+            return self
+        return self._from_parsed_parts(drv, root, parts[:-1], **self._kwargs)
+
+    def stat(self):
+        return self._accessor.stat(self)
 
     def iterdir(self):
         """Iterate over the files in this directory.  Does not yield any
@@ -302,29 +317,49 @@ class UPath(pathlib.Path, PureUPath, metaclass=UPathMeta):
         self._accessor.rm(self, recursive=recursive)
 
     @classmethod
-    def _from_parts_init(cls, args, init=False):
-        return super()._from_parts(args, init=init)
+    def _parse_args(cls, args, **kwargs):
+        return super(UPath, cls)._parse_args(args)
 
-    def _from_parts(self, args, init=True):
-        # We need to call _parse_args on the instance, so as to get the
-        # right flavour.
-        obj = object.__new__(self.__class__)
-        drv, root, parts = self._parse_args(args)
+    @classmethod
+    def _from_parts(cls, args, **kwargs):
+        obj = object.__new__(cls)
+        drv, root, parts = obj._parse_args(args, **kwargs)
         obj._drv = drv
-        obj._root = root
         obj._parts = parts
-        if init:
-            obj._init(**self._kwargs)
+        obj._closed = False
+        obj._kwargs = kwargs.copy()
+        obj._url = kwargs.pop("_url", None) or None
+
+        if not root:
+            if not parts:
+                root = "/"
+            elif parts[0] == "/":
+                root = parts.pop(0)
+        obj._root = root
+
         return obj
 
-    def _from_parsed_parts(self, drv, root, parts, init=True):
-        obj = object.__new__(self.__class__)
+    @classmethod
+    def _from_parsed_parts(cls, drv, root, parts, **kwargs):
+        obj = object.__new__(cls)
         obj._drv = drv
-        obj._root = root
         obj._parts = parts
-        if init:
-            obj._init(**self._kwargs)
+        obj._closed = False
+        obj._kwargs = kwargs.copy()
+        obj._url = kwargs.pop("_url", None) or None
+
+        if not root:
+            if not parts:
+                root = "/"
+            elif parts[0] == "/":
+                root = parts.pop(0)
+        obj._root = root
+
         return obj
+
+    @property
+    def fs(self):
+        return self._accessor._fs
 
     def __truediv__(self, key):
         # Add `/` root if not present
@@ -351,9 +386,6 @@ class UPath(pathlib.Path, PureUPath, metaclass=UPathMeta):
         kwargs = state["_kwargs"].copy()
         kwargs["_url"] = self._url
         self._kwargs = kwargs
-        # _init needs to be called again, because when __new__ called _init,
-        # the _kwargs were not yet set
-        self._init()
 
     def __reduce__(self):
         kwargs = self._kwargs.copy()
