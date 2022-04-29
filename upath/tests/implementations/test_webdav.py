c4832bb1592de535ebfda17806782b962de840b8
Norman Rzepka <code@normanrz.com>
2022-04-29 11:40:48 +0200

    windows?

--- upath/tests/implementations/test_webdav.py
+++ upath/tests/implementations/test_webdav.py
@@ -2,10 +2,8 @@ import pytest  # noqa: F401
 
 from upath import UPath
 from ..cases import BaseTests
-from ..utils import skip_on_windows
 
 
-@skip_on_windows
 class TestUPathWebdav(BaseTests):
     @pytest.fixture(autouse=True, scope="function")
     def path(self, webdav_fixture):
