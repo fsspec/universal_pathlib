67d5d402b2af54cbe00d612492e658d9b2aff5ca
Andreas Poehlmann <andreas@poehlmann.io>
2022-09-19 09:32:58 +0200

    Lazy registry (#78)
    * upath.registry: lazy import registry and cleanup UPath.__new__
    
    * tests: explicitly test file protocol no warning

--- upath/registry.py
+++ upath/registry.py
@@ -1,40 +1,72 @@
-from typing import Dict, Type
+import importlib
 import warnings
+from functools import lru_cache
+from typing import TYPE_CHECKING
+from typing import TypeVar
 
-import upath
-from upath.core import UPath
+from fsspec.core import get_filesystem_class
+
+if TYPE_CHECKING:
+    from upath.core import UPath
+
+__all__ = [
+    "get_upath_class",
+]
 
 
 class _Registry:
-    from upath.implementations import cloud, hdfs, http, memory, webdav
-
-    known_implementations: Dict[str, Type[UPath]] = {
-        "abfs": cloud.AzurePath,
-        "adl": cloud.AzurePath,
-        "az": cloud.AzurePath,
-        "gcs": cloud.GCSPath,
-        "gs": cloud.GCSPath,
-        "hdfs": hdfs.HDFSPath,
-        "http": http.HTTPPath,
-        "https": http.HTTPPath,
-        "memory": memory.MemoryPath,
-        "s3": cloud.S3Path,
-        "s3a": cloud.S3Path,
-        "webdav+http": webdav.WebdavPath,
-        "webdav+https": webdav.WebdavPath,
+    known_implementations: "dict[str, type[UPath]]" = {
+        "abfs": "upath.implementations.cloud.AzurePath",
+        "adl": "upath.implementations.cloud.AzurePath",
+        "az": "upath.implementations.cloud.AzurePath",
+        "gcs": "upath.implementations.cloud.GCSPath",
+        "gs": "upath.implementations.cloud.GCSPath",
+        "hdfs": "upath.implementations.hdfs.HDFSPath",
+        "http": "upath.implementations.http.HTTPPath",
+        "https": "upath.implementations.http.HTTPPath",
+        "memory": "upath.implementations.memory.MemoryPath",
+        "s3": "upath.implementations.cloud.S3Path",
+        "s3a": "upath.implementations.cloud.S3Path",
+        "webdav+http": "upath.implementations.webdav.WebdavPath",
+        "webdav+https": "upath.implementations.webdav.WebdavPath",
     }
 
-    def __getitem__(self, item):
-        implementation = self.known_implementations.get(item, None)
-        if not implementation:
-            warning_str = (
-                f"{item} filesystem path not explicitly implemented. "
-                "falling back to default implementation. "
-                "This filesystem may not be tested"
-            )
-            warnings.warn(warning_str, UserWarning)
-            return upath.UPath
-        return implementation
+    def __getitem__(self, item: str) -> "type[UPath] | None":
+        try:
+            fqn = self.known_implementations[item]
+        except KeyError:
+            return None
+        mod, name = fqn.rsplit(".", 1)
+        mod = importlib.import_module(mod)
+        return getattr(mod, name)
 
 
 _registry = _Registry()
+
+_T = TypeVar("_T", bound="UPath")
+
+
+@lru_cache()
+def get_upath_class(protocol: str) -> "type[_T] | None":
+    """return the upath cls for the given protocol"""
+    cls = _registry[protocol]
+    if cls is not None:
+        return cls
+    else:
+        if not protocol:
+            return None  # we want to use pathlib for `None` protocol
+        try:
+            _fs_cls = get_filesystem_class(protocol)
+        except ValueError:
+            return None  # this is an unknown protocol
+        else:
+            if _fs_cls.protocol != "file":
+                warnings.warn(
+                    f"UPath {protocol!r} filesystem not explicitly implemented."
+                    " Falling back to default implementation."
+                    " This filesystem may not be tested.",
+                    UserWarning,
+                    stacklevel=2,
+                )
+            mod = importlib.import_module("upath.core")
+            return getattr(mod, "UPath")
