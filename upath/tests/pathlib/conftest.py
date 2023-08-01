e9ad0f693d753b81111a3f0616a6104782a89e97
ap-- <andreas@poehlmann.io>
2023-08-01 22:33:11 +0200

    tests: pathlib clean collection

---
+++ upath/tests/pathlib/conftest.py
@@ -0,0 +1,27 @@
+import sys
+
+BASE_URL = "https://raw.githubusercontent.com/python/cpython"
+
+# current origin of pathlib tests:
+TEST_FILES = {
+    "test_pathlib_38.py":  f"{BASE_URL}/7475aa2c590e33a47f5e79e4079bca0645e93f2f/Lib/test/test_pathlib.py",
+    "test_pathlib_39.py":  f"{BASE_URL}/d718764f389acd1bf4a5a65661bb58862f14fb98/Lib/test/test_pathlib.py",
+    "test_pathlib_310.py": f"{BASE_URL}/b382bf50c53e6eab09f3e3bf0802ab052cb0289d/Lib/test/test_pathlib.py",
+    "test_pathlib_311.py": f"{BASE_URL}/846a23d0b8f08e62a90682c51ce01301eb923f2e/Lib/test/test_pathlib.py",
+    "test_pathlib_312.py": f"{BASE_URL}/97a6a418167f1c8bbb014fab813e440b88cf2221/Lib/test/test_pathlib.py",  # 3.12.0b4
+}
+
+
+def pytest_ignore_collect(collection_path, path, config):
+    """prevents pathlib tests from other python version than the current to be collected
+
+    (otherwise we see a lot of skipped tests in the pytest output)
+    """
+    v2 = sys.version_info[:2]
+    return {
+        "test_pathlib_38.py": v2 != (3, 8),
+        "test_pathlib_39.py": v2 != (3, 9),
+        "test_pathlib_310.py": v2 != (3, 10),
+        "test_pathlib_311.py": v2 != (3, 11),
+        "test_pathlib_312.py": v2 != (3, 12),
+    }.get(collection_path.name, False)
