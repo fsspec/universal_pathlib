d2a774b761e31a127aac25d511d241058ac7a4b5
Andreas Poehlmann <ap--@users.noreply.github.com>
2023-10-04 10:33:40 +0200

    tests: add missing nox session

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ nox.options.sessions = "lint", "tests"
 locations = ("upath",)
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "pypy3.8", "pypy3.9"])
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "pypy3.8", "pypy3.9"])
 def tests(session: nox.Session) -> None:
     session.install(".[dev]")
     session.run(
