2a72eb4ccc673c639bc7d949c08a70f80d21e7ee
Andreas Poehlmann <ap--@users.noreply.github.com>
2024-09-08 21:44:58 +0200

    tests: enable local tests on windows

--- upath/tests/implementations/test_local.py
+++ upath/tests/implementations/test_local.py
@@ -3,11 +3,9 @@ import pytest
 from upath import UPath
 from upath.implementations.local import LocalPath
 from upath.tests.cases import BaseTests
-from upath.tests.utils import skip_on_windows
 from upath.tests.utils import xfail_if_version
 
 
-@skip_on_windows
 class TestFSSpecLocal(BaseTests):
     @pytest.fixture(autouse=True)
     def path(self, local_testdir):
@@ -18,7 +16,6 @@ class TestFSSpecLocal(BaseTests):
         assert isinstance(self.path, LocalPath)
 
 
-@skip_on_windows
 @xfail_if_version("fsspec", lt="2023.10.0", reason="requires fsspec>=2023.10.0")
 class TestRayIOFSSpecLocal(BaseTests):
     @pytest.fixture(autouse=True)
