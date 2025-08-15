2da9d050b0ec7aba7c4d355b842d2ae05f7a1db7
Philip Meier <github.pmeier@posteo.de>
2025-08-15 16:17:22 +0200

    parametrize tests

--- upath/tests/test_pydantic.py
+++ upath/tests/test_pydantic.py
@@ -4,76 +4,110 @@ from os.path import abspath
 import pydantic
 import pydantic_core
 import pytest
+from fsspec.implementations.http import get_client
 
 from upath import UPath
 
 
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/abc",
+        "file:///abc",
+        "memory://abc",
+        "s3://bucket/key",
+        "https://www.example.com",
+    ],
+)
 @pytest.mark.parametrize("source", ["json", "python"])
-def test_validate_from_str(source):
-    input = "file:///my/path"
+def test_validate_from_str(path, source):
+    expected = UPath(path)
 
     ta = pydantic.TypeAdapter(UPath)
     if source == "json":
-        output = ta.validate_json(json.dumps(input))
+        actual = ta.validate_json(json.dumps(path))
     else:  # source == "python"
-        output = ta.validate_python(input)
+        actual = ta.validate_python(path)
 
-    u = UPath(input)
-    assert abspath(output.path) == abspath(u.path)
-    assert output.protocol == u.protocol
+    assert abspath(actual.path) == abspath(expected.path)
+    assert actual.protocol == expected.protocol
 
 
+@pytest.mark.parametrize(
+    "dct",
+    [
+        {
+            "path": "/my/path",
+            "protocol": "file",
+            "storage_options": {"foo": "bar", "baz": 3},
+        }
+    ],
+)
 @pytest.mark.parametrize("source", ["json", "python"])
-def test_validate_from_dict(source):
-    input = {
-        "path": "/my/path",
-        "protocol": "file",
-        "storage_options": {"foo": "bar", "baz": 3},
-    }
-
+def test_validate_from_dict(dct, source):
     ta = pydantic.TypeAdapter(UPath)
     if source == "json":
-        output = ta.validate_json(json.dumps(input))
+        output = ta.validate_json(json.dumps(dct))
     else:  # source == "python"
-        output = ta.validate_python(input)
+        output = ta.validate_python(dct)
 
-    assert abspath(output.path) == abspath(input["path"])
-    assert output.protocol == input["protocol"]
-    assert output.storage_options == input["storage_options"]
+    assert abspath(output.path) == abspath(dct["path"])
+    assert output.protocol == dct["protocol"]
+    assert output.storage_options == dct["storage_options"]
 
 
-def test_validate_from_instance():
-    input = UPath("/my/path")
+@pytest.mark.parametrize(
+    "path",
+    [
+        "/abc",
+        "file:///abc",
+        "memory://abc",
+        "s3://bucket/key",
+        "https://www.example.com",
+    ],
+)
+def test_validate_from_instance(path):
+    input = UPath(path)
 
     output = pydantic.TypeAdapter(UPath).validate_python(input)
 
     assert output is input
 
 
+@pytest.mark.parametrize(
+    ("args", "kwargs"),
+    [
+        (
+            ("/my/path",),
+            {
+                "protocol": "file",
+                "foo": "bar",
+                "baz": 3,
+            },
+        )
+    ],
+)
 @pytest.mark.parametrize("mode", ["json", "python"])
-def test_dump(mode):
-    input = UPath("/my/path", protocol="file", foo="bar", baz=3)
+def test_dump(args, kwargs, mode):
+    u = UPath(*args, **kwargs)
 
-    output = pydantic.TypeAdapter(UPath).dump_python(input, mode=mode)
+    output = pydantic.TypeAdapter(UPath).dump_python(u, mode=mode)
 
-    assert output["path"] == input.path
-    assert output["protocol"] == input.protocol
-    assert output["storage_options"] == input.storage_options
+    assert output["path"] == u.path
+    assert output["protocol"] == u.protocol
+    assert output["storage_options"] == u.storage_options
 
 
 def test_dump_non_serializable_python():
-    input = UPath("/my/path", protocol="file", non_serializable=object())
-
-    output = pydantic.TypeAdapter(UPath).dump_python(input, mode="python")
-
-    assert (
-        output["storage_options"]["non_serializable"]
-        is input.storage_options["non_serializable"]
+    output = pydantic.TypeAdapter(UPath).dump_python(
+        UPath("https://www.example.com", get_client=get_client), mode="python"
     )
+
+    assert output["storage_options"]["get_client"] is get_client
 
 
 def test_dump_non_serializable_json():
-    input = UPath("/my/path", protocol="file", non_serializable=object())
-
     with pytest.raises(pydantic_core.PydanticSerializationError, match="unknown type"):
-        pydantic.TypeAdapter(UPath).dump_python(input, mode="json")
+        pydantic.TypeAdapter(UPath).dump_python(
+            UPath("https://www.example.com", get_client=get_client), mode="json"
+        )
