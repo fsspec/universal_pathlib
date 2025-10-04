5e24a1c5522b5ae683e4574761ca4e15fcac06e8
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-10-04 20:50:55 +0200

    upath.registry: overload get_upath_class annotations to return correct subclass

--- upath/implementations/_experimental.py
+++ upath/implementations/_experimental.py
@@ -10,8 +10,17 @@ if TYPE_CHECKING:
 
 def __getattr__(name: str) -> type[UPath]:
     if name.startswith("_") and name.endswith("Path"):
+        from upath import UPath
+
         protocol = name[1:-4].lower()
-        cls = get_upath_class(protocol, fallback=False)
-        assert cls is not None
+        cls = get_upath_class(protocol)
+        if cls is None:
+            raise RuntimeError(
+                f"Could not find fsspec implementation for protocol {protocol!r}"
+            )
+        elif not issubclass(cls, UPath):
+            raise RuntimeError(
+                "UPath implementation not a subclass of upath.UPath, {cls!r}"
+            )
         return cls
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
