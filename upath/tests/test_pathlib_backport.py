331b3eefa2e3b571728d1200df8793d87a475943
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-11-11 23:57:15 +0100

    tests: add backport tests

--- upath/tests/test_pathlib_backport.py
+++ upath/tests/test_pathlib_backport.py
@@ -41,3 +41,72 @@ def pth(request, tmp_path):
 
 def test_signature_stat_follow_symlinks(pth):
     _ = pth.stat(follow_symlinks=True)
+
+
+def test_signature_write_text_newline(pth):
+    _ = pth.write_text("test", newline="\n")
+
+
+def test_signature_chmod_follow_symlinks(pth):
+    _ = pth.chmod(0o777, follow_symlinks=True)
+
+
+def test_signature_match_pathlike(pth):
+    _ = pth.match(UPath("*.py"))
+
+
+def test_signature_match_case_sensitive(pth):
+    _ = pth.match("*.py", case_sensitive=True)
+
+
+def test_signature_exists_follow_symlinks(pth):
+    _ = pth.exists(follow_symlinks=True)
+
+
+def test_signature_glob_case_sensitive(pth):
+    _ = list(pth.parent.glob("*", case_sensitive=True))
+
+
+def test_signature_rglob_case_sensitive(pth):
+    _ = list(pth.parent.rglob("*", case_sensitive=True))
+
+
+def test_signature_relative_to_walk_up(pth):
+    _ = pth.relative_to(pth.parent.parent, walk_up=False)
+
+
+def test_signature_is_file_follow_symlinks(pth):
+    _ = pth.is_file(follow_symlinks=True)
+
+
+def test_signature_is_dir_follow_symlinks(pth):
+    _ = pth.is_dir(follow_symlinks=True)
+
+
+def test_signature_read_text_newline(pth):
+    pth.write_text("test")
+    _ = pth.read_text(newline="\n")
+
+
+def test_signature_glob_recurse_symlinks(pth):
+    _ = list(pth.parent.glob("**/*.py", recurse_symlinks=True))
+
+
+def test_signature_glob_pathlike(pth):
+    _ = list(pth.parent.glob(UPath("**/*.py")))
+
+
+def test_signature_rglob_recurse_symlinks(pth):
+    _ = list(pth.parent.rglob("*.py", recurse_symlinks=True))
+
+
+def test_signature_rglob_pathlike(pth):
+    _ = list(pth.parent.rglob(UPath("*.py")))
+
+
+def test_signature_owner_follow_symlinks(pth):
+    _ = pth.owner(follow_symlinks=True)
+
+
+def test_signature_group_follow_symlinks(pth):
+    _ = pth.group(follow_symlinks=True)
