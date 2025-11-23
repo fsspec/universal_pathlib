506aba9a833d30befe44fc8a601e76348f01f6ad
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-11-23 18:30:33 +0100

    tests: add test for mtime for ftp filesystem

--- upath/tests/implementations/test_ftp.py
+++ upath/tests/implementations/test_ftp.py
@@ -12,3 +12,10 @@ class TestUPathFTP(BaseTests):
     def path(self, ftp_server):
         self.path = UPath("", protocol="ftp", **ftp_server)
         self.prepare_file_system()
+
+
+def test_ftp_path_mtime(ftp_server):
+    path = UPath("file1.txt", protocol="ftp", **ftp_server)
+    path.touch()
+    mtime = path.stat().st_mtime
+    assert isinstance(mtime, float)
