3f1ff7f49717a41bde7eb7d658b531b61e2c5135
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-12-21 22:12:52 +0100

    tests: cleanup s3 tests

--- upath/tests/implementations/test_s3.py
+++ upath/tests/implementations/test_s3.py
@@ -3,12 +3,15 @@
 import sys
 
 import fsspec
-import pytest  # noqa: F401
+import pytest
 
 from upath import UPath
 from upath.implementations.cloud import S3Path
 
 from ..cases import BaseTests
+from ..utils import OverrideMeta
+from ..utils import extends_base
+from ..utils import overrides_base
 
 
 def silence_botocore_datetime_deprecation(cls):
@@ -25,7 +28,7 @@ def silence_botocore_datetime_deprecation(cls):
 
 
 @silence_botocore_datetime_deprecation
-class TestUPathS3(BaseTests):
+class TestUPathS3(BaseTests, metaclass=OverrideMeta):
     SUPPORTS_EMPTY_DIRS = False
 
     @pytest.fixture(autouse=True)
@@ -35,13 +38,11 @@ class TestUPathS3(BaseTests):
         self.anon = anon
         self.s3so = s3so
 
-    def test_is_S3Path(self):
+    @overrides_base
+    def test_is_correct_class(self):
         assert isinstance(self.path, S3Path)
 
-    def test_chmod(self):
-        # todo
-        pass
-
+    @extends_base
     def test_rmdir(self):
         dirname = "rmdir_test"
         mock_dir = self.path.joinpath(dirname)
@@ -51,11 +52,13 @@ class TestUPathS3(BaseTests):
         with pytest.raises(NotADirectoryError):
             self.path.joinpath("file1.txt").rmdir()
 
-    def test_relative_to(self):
+    @extends_base
+    def test_relative_to_extra(self):
         assert "file.txt" == str(
             UPath("s3://test_bucket/file.txt").relative_to(UPath("s3://test_bucket"))
         )
 
+    @extends_base
     def test_iterdir_root(self):
         client_kwargs = self.path.storage_options["client_kwargs"]
         bucket_path = UPath("s3://other_test_bucket", client_kwargs=client_kwargs)
@@ -68,6 +71,7 @@ class TestUPathS3(BaseTests):
             assert x.name != ""
             assert x.exists()
 
+    @extends_base
     @pytest.mark.parametrize(
         "joiner", [["bucket", "path", "file"], ["bucket/path/file"]]
     )
@@ -76,10 +80,12 @@ class TestUPathS3(BaseTests):
         path = path.joinpath(*joiner)
         assert str(path) == "s3://bucket/path/file"
 
+    @extends_base
     def test_creating_s3path_with_bucket(self):
         path = UPath("s3://", bucket="bucket", anon=self.anon, **self.s3so)
         assert str(path) == "s3://bucket/"
 
+    @extends_base
     def test_iterdir_with_plus_in_name(self, s3_with_plus_chr_name):
         bucket, anon, s3so = s3_with_plus_chr_name
         p = UPath(
@@ -93,6 +99,7 @@ class TestUPathS3(BaseTests):
         (file,) = files
         assert file == p.joinpath("file.txt")
 
+    @extends_base
     @pytest.mark.xfail(reason="fsspec/universal_pathlib#144")
     def test_rglob_with_double_fwd_slash(self, s3_with_double_fwd_slash_files):
         import boto3
