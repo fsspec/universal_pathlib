5d45e4fd0f40489c49f477f2abd6f78786378ec7
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-12-21 21:48:08 +0100

    tests: cleanup cached tests

--- upath/tests/implementations/test_cached.py
+++ upath/tests/implementations/test_cached.py
@@ -4,9 +4,11 @@ from upath import UPath
 from upath.implementations.cached import SimpleCachePath
 
 from ..cases import BaseTests
+from ..utils import OverrideMeta
+from ..utils import overrides_base
 
 
-class TestSimpleCachePath(BaseTests):
+class TestSimpleCachePath(BaseTests, metaclass=OverrideMeta):
     @pytest.fixture(autouse=True)
     def path(self, local_testdir):
         if not local_testdir.startswith("/"):
@@ -15,5 +17,6 @@ class TestSimpleCachePath(BaseTests):
         self.path = UPath(path)
         self.prepare_file_system()
 
-    def test_is_SimpleCachePath(self):
+    @overrides_base
+    def test_is_correct_class(self):
         assert isinstance(self.path, SimpleCachePath)
