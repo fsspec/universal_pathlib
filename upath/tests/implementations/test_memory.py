5f44e92aad1f9118c459da7fadf800ecc393f556
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-12-21 22:07:52 +0100

    tests: cleanup memory tests

--- upath/tests/implementations/test_memory.py
+++ upath/tests/implementations/test_memory.py
@@ -4,9 +4,11 @@ from upath import UPath
 from upath.implementations.memory import MemoryPath
 
 from ..cases import BaseTests
+from ..utils import OverrideMeta
+from ..utils import overrides_base
 
 
-class TestMemoryPath(BaseTests):
+class TestMemoryPath(BaseTests, metaclass=OverrideMeta):
     @pytest.fixture(autouse=True)
     def path(self, local_testdir):
         if not local_testdir.startswith("/"):
@@ -15,7 +17,8 @@ class TestMemoryPath(BaseTests):
         self.path = UPath(path)
         self.prepare_file_system()
 
-    def test_is_MemoryPath(self):
+    @overrides_base
+    def test_is_correct_class(self):
         assert isinstance(self.path, MemoryPath)
 
 
