52ca3feab975ead1fc17989826aa6dbf4e1cd0ea
Andreas Poehlmann <ap--@users.noreply.github.com>
2025-12-28 15:10:44 +0100

    tests: cleanup huggingface tests

--- upath/tests/implementations/test_hf.py
+++ upath/tests/implementations/test_hf.py
@@ -1,10 +1,15 @@
 import pytest
 from fsspec import get_filesystem_class
 
+from upath import UnsupportedOperation
 from upath import UPath
 from upath.implementations.cloud import HfPath
 
-from ..cases import BaseTests
+from ..cases import JoinablePathTests
+from ..cases import NonWritablePathTests
+from ..cases import ReadablePathTests
+from ..utils import OverrideMeta
+from ..utils import overrides_base
 
 try:
     get_filesystem_class("hf")
@@ -31,84 +36,22 @@ def test_hfpath():
             raise
 
 
-class TestUPathHf(BaseTests):
+class TestUPathHf(
+    JoinablePathTests,
+    ReadablePathTests,
+    NonWritablePathTests,
+    metaclass=OverrideMeta,
+):
     @pytest.fixture(autouse=True, scope="function")
     def path(self, hf_fixture_with_readonly_mocked_hf_api):
         self.path = UPath(hf_fixture_with_readonly_mocked_hf_api)
 
-    @pytest.mark.skip
-    def test_mkdir(self):
-        pass
+    @overrides_base
+    def test_is_correct_class(self):
+        assert isinstance(self.path, HfPath)
 
-    @pytest.mark.skip
-    def test_mkdir_exists_ok_false(self):
-        pass
-
-    @pytest.mark.skip
-    def test_mkdir_exists_ok_true(self):
-        pass
-
-    @pytest.mark.skip
-    def test_mkdir_parents_true_exists_ok_true(self):
-        pass
-
-    @pytest.mark.skip
-    def test_mkdir_parents_true_exists_ok_false(self):
-        pass
-
-    @pytest.mark.skip
-    def test_makedirs_exist_ok_true(self):
-        pass
-
-    @pytest.mark.skip
-    def test_makedirs_exist_ok_false(self):
-        pass
-
-    @pytest.mark.skip
-    def test_touch(self):
-        pass
-
-    @pytest.mark.skip
-    def test_touch_unlink(self):
-        pass
-
-    @pytest.mark.skip
-    def test_write_bytes(self, pathlib_base):
-        pass
-
-    @pytest.mark.skip
-    def test_write_text(self, pathlib_base):
-        pass
-
-    def test_fsspec_compat(self):
-        pass
-
-    def test_rename(self):
-        pass
-
-    def test_rename2(self):
-        pass
-
-    def test_move_local(self, tmp_path):
-        pass
-
-    def test_move_into_local(self, tmp_path):
-        pass
-
-    def test_move_memory(self, clear_fsspec_memory_cache):
-        pass
-
-    def test_move_into_memory(self, clear_fsspec_memory_cache):
-        pass
-
-    @pytest.mark.skip(reason="HfPath does not support listing repositories")
-    def test_iterdir(self, local_testdir):
-        pass
-
-    @pytest.mark.skip(reason="HfPath does not support listing repositories")
-    def test_iterdir2(self, local_testdir):
-        pass
-
-    @pytest.mark.skip(reason="HfPath does not currently test write")
-    def test_rename_with_target_absolute(self, target_factory):
-        return super().test_rename_with_target_absolute(target_factory)
+    @overrides_base
+    def test_iterdir_parent_iteration(self):
+        # HfPath does not support listing all available Repositories
+        with pytest.raises(UnsupportedOperation):
+            super().test_iterdir_parent_iteration()
